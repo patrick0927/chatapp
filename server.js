feddb6958643ce9ade654020e757ceea9469a091
= <patrick0927@gmail.com>
2016-06-11 15:44:02 -0400

    Initialize

--- server.js
+++ server.js
@@ -2,10 +2,27 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.get('/', function (req, res){
-    res.send('Chat App API root');
+//Middleware
+var middleware = {
+    requireAuthentication: function(req, res, next){
+        console.log('Private route hit!');
+        next();
+    },
+    logger: function(req, res, next){
+        console.log( new Date().toString() +': Request: ' + req.method + ' ' + req.originalUrl );
+        next();
+    }
+};
+
+app.use(middleware.logger);
+// app.use(middleware.requireAuthentication);
+
+app.get('/about', middleware.requireAuthentication, function (req, res){
+    res.send('About us');
 });
 
+app.use(express.static(__dirname + '/public'));
+
 app.listen(PORT, function(){
-    console.log('Express listening on port ' + PORT + '!' );
+    console.log('Express server started on port ' + PORT + '!');
 });
