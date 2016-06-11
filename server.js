707561b7bed05ffd6fec8e4832e79a3636fff457
= <patrick0927@gmail.com>
2016-06-11 15:56:44 -0400

    Refactor middleware

--- server.js
+++ server.js
@@ -3,16 +3,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 //Middleware
-var middleware = {
-    requireAuthentication: function(req, res, next){
-        console.log('Private route hit!');
-        next();
-    },
-    logger: function(req, res, next){
-        console.log( new Date().toString() +': Request: ' + req.method + ' ' + req.originalUrl );
-        next();
-    }
-};
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 // app.use(middleware.requireAuthentication);
